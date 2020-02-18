5bd633f44ae52b3bad815dc9fd9fd8d53dc58b2f
AnyxLeo <anyx.leo@gmail.com>
2020-02-18 12:22:21 -0600

    wip

--- CallbacksExercise.js
+++ CallbacksExercise.js
@@ -1,47 +1,46 @@
 var responses = {};
-
 function fakeAjax(url, cb) {
-    const fakeResponses = {
-        file1: "The first text",
-        file2: "The second text",
-        file3: "The third text",
-    }
+  const fakeResponses = {
+    file1: "The first text",
+    file2: "The second text",
+    file3: "The third text"
+  };
 
-    let randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+  let randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
-    console.log(`Requesting: ${url}`);
+  console.log(`Requesting: ${url}`);
 
-    setTimeout(() => cb(fakeResponses[url]), randomDelay);
+  setTimeout(() => cb(url, fakeResponses[url]), randomDelay);
 }
 
 function output(text) {
-    console.log(text);
-}
-
-function getFile(file) {
-    fakeAjax(file, function (text) {
-        handleResponses(file.text);
-    });
+  console.log(text);
 }
 
 function handleResponses(fileName, content) {
-    if (!(fileName in responses))
-        responses[fileName] = content;
+  if (!(fileName in responses)) {
+    responses[fileName] = content;
+  }
 
-    var fileNames = ["file1", "file2", "file3"];
+  var fileNames = ["file1", "file2", "file3"];
 
-    for (let i = 0; i < fileNames.length; i++) {
-        if (fileNames[i] in responses) {
-            if (typeof responses[i] == "string") {
-                output(responses[fileNames[i]]);
-                responses[fileNames[i]] = false;
-            }
-        }
-        else {
-            return;
-        }
+  for (let i = 0; i < fileNames.length; i++) {
+    if (fileNames[i] in responses) {
+      if (typeof( responses[fileNames[i]] ) === "string") {
+        output(responses[fileNames[i]]);
+        responses[fileNames[i]] = false;
+      }
+    } else {
+      return;
     }
-    output("Complete!");
+  }
+  output("Complete!");
+}
+
+function getFile(fileName) {
+  fakeAjax(fileName, function(text) {
+    handleResponses(fileName, text);
+  });
 }
 
 getFile("file1");
